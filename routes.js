3d96427ae33bb81627737f88ecb045441d4ba7c0
ZakariaZermani <43765819+zakariaabdelhadi@users.noreply.github.com>
2023-05-24 23:16:33 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -8,12 +8,12 @@ router.get("/wetter", (req, res) => {
 
     // install node-fetch module
     //this link is to use for forecast 
-    //let apiLink = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&exclude=hourly,daily&appid=d11ad90a4ab6e0b72bf65e5ce7970f92';
+    //let apiLink = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&exclude=hourly,daily&appid=${{secrets.OPEN_WHEATHER_API}}';
 
     // Berlin location
     let lon = 13.4105;
     let lat = 52.5244;
-    let apiLink = `https://api.openweathermap.org/data/2.5/onecall?lon=${lon}&lat=${lat}&appid=d11ad90a4ab6e0b72bf65e5ce7970f92&exclude=hourly,current,minutely,alerts&units=metric`;
+    let apiLink = `https://api.openweathermap.org/data/2.5/onecall?lon=${lon}&lat=${lat}&appid=${{secrets.OPEN_WHEATHER_API}}&exclude=hourly,current,minutely,alerts&units=metric`;
 
     fetch(apiLink)
         .then(fetchres => fetchres.json())
@@ -46,7 +46,7 @@ router.get("/wetter-current", (req, res) => {
     // install node-fetch module
     //this link is to use for forecast 
     let stadt = 'berlin'
-    let apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=' + stadt + '&appid=d11ad90a4ab6e0b72bf65e5ce7970f92&units=metric';
+    let apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=' + stadt + '&appid=${{secrets.OPEN_WHEATHER_API}}&units=metric';
     fetch(apiLink)
         .then(fetchres => fetchres.json())
         .then(json => {
